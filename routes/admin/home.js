bfa6847a506ab48bdec17b9b8391b8bc24db2372
Frank <frank@FrankdeMacBook-Air.local>
2016-04-07 13:50:16 +0800

    complete project add view

--- routes/admin/home.js
+++ routes/admin/home.js
@@ -5,7 +5,7 @@ var adminbaserender = require('../../lib/middlewares/adminbaserender');
 
 //登录
 exports.index = function *() {
-    yield adminbaserender(this, "admin/home", {
-        title: 'home'
+    yield adminbaserender(this,"admin/index", {
+        title: '后台首页'
     });
 }
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,15 +1,13 @@
 var UserModel = require('../../viewmodels/admin/adminuser');
 var $ = require('../../lib/util/md5');
 var iphelper = require('../../lib/util/iphelper.js');
+var adminbaserender = require('../../lib/middlewares/adminbaserender');
 
 exports.index = function*() {
 
 
-    yield render(this, "admin/index", {
+    yield adminbaserender(this, "admin/index", {
         // title: JSON.stringify(this.request)
-        title:'ffff',
-        layout: 'admin_layout'
-
-
+        title:'ffff'
     });
 }
