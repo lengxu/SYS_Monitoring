00c14f1657e52c14f107adf68032e262300cbeb7
Frank <frank@192.168.2.100>
2016-04-14 20:53:44 +0800

    project detail

--- routes/wechat/index.js
+++ routes/wechat/index.js
@@ -76,19 +76,18 @@ exports.settestlogin = function*() {
 
     var wechatuser = new WechatUserModel();
 
-    wechatuser.openid = 'testopenid';
+    wechatuser.openid = 'testopenid1';
 
     //检查是否存在该用户
     var result = yield thunkify(WechatUserModel.findByOpenID, WechatUserModel)(wechatuser.openid);
 
-    console.log(result);
     if (result) {
 
         wechatuser.updatetime = new Date().toFormat("YYYY-MM-DD HH24:MI:SS");
 
         this.session.wechatUserInfo = wechatuser;
 
-        // result = yield thunkify(result.update, result)(wechatuser);
+        result = yield thunkify(result.update, result)(result);
 
     }
     else {
@@ -103,6 +102,4 @@ exports.settestlogin = function*() {
     }
 
     this.body = '模拟登录成功';
-
-
 }
