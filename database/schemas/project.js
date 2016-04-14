8162331206502a71ebfe36466d3088199786e9d4
Frank <frank@FrankdeMacBook-Air.local>
2016-04-14 17:42:53 +0800

    project admin complete

--- database/schemas/project.js
+++ database/schemas/project.js
@@ -30,6 +30,46 @@ ProjectSchema.statics = {
 
 };
 
+ProjectSchema.methods.GetStatus = function () {
+    
+    if (this.status==0)
+    {
+
+        return '已启用';
+    }
+    else if(this.status==-1)
+    {
+
+        return '未启用';
+    }
+    else{
+        return '未知状态';
+    }
+};
+
+
+ProjectSchema.methods.GetMonitortingStatus = function () {
+
+    if (this.lastmonitorstatus==0)
+    {
+
+        return '正常';
+    }
+    else if(this.lastmonitorstatus==-1)
+    {
+
+        return '出错';
+    }
+    else{
+        return '未知状态';
+    }
+};
+
+// Schema.method('GetStatus', function () {
+//     return 'sdfdf'
+// });
+
+
 ProjectSchema.plugin(koamongoosePagination);
 
 module.exports = ProjectSchema;
