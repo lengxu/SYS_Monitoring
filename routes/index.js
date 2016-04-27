755fca477532e74342ce5aed1b9ead05c133c3e8
Frank <frank@192.168.0.102>
2016-04-28 01:24:06 +0800

    qrcod

--- routes/index.js
+++ routes/index.js
@@ -7,47 +7,15 @@ var http = require('http');
 //登录
 exports.index = function *(){
 
-    //get request
-    var nodegrass = require('nodegrass');
+    var qrcode = require('node-qrcode');
 
-    nodegrass.get("http://www.ompchinqa.com/asdfasd",function(data,status,headers){
-        console.log(status);
-        console.log(headers);
-        console.log(data);
-    },null,'utf8').on('error', function(e) {
-        console.log("Got error: " + e.message);
+    qrcode({
+        text: 'http://weibo.com',
+        size: 200,
+        qrcodePath: './public/upload/qrcode/qrcode22.png'
+    }).then(function(qrcodePath) {
+        console.log(qrcodePath);  // balabala/node-qrcode/qrcode.png
     });
 
-    // console.log('111111122222222');
-    //
-    //
-    // var qs = require('querystring');
-    //
-    // var post_data = {
-    //     a: 123,
-    //     time: new Date().getTime()};//这是需要提交的数据
-    //
-    //
-    // var content = qs.stringify(post_data);
-    //
-    // var options = {
-    //     hostname: 'www.ompchina.com',
-    //     port: 80,
-    //     path: '/',
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    //     }
-    // };
-    //
-    // var req = http.request(options, function (res) {
-    //     console.log('STATUS: ' + res.statusCode);
-    //     console.log('HEADERS: ' + JSON.stringify(res.headers));
-    // });
-    //
-    // req.on('error', function (e) {
-    //     console.log('problem with request: ' + e.message);
-    // });
-    //
-    // console.log(req);
+    this.body='111111';
 }
--- routes/project/index.js
+++ routes/project/index.js
@@ -16,13 +16,31 @@ exports.detail = function*() {
     var browser = require('beyond-lib/lib/browser');
     var browserinfo = browser.parse(this.request.header["user-agent"]);
 
+    //判断微信跳转
     if(browserinfo.isMicroMessenger)
     {
         this.redirect('/member/project/'+info.id+"/detail");
         return;
     }
 
+    //获取项目详情
     var projectinfo = yield thunkify(ProjectModel.findByid, ProjectModel)(info.id);
+
+    var qrcodeurl='';
+
+    //生成二维码
+    var qrcode = require('node-qrcode'),
+        siteinfo = require('../../config').siteinfo;
+
+    qrcode({
+        text: siteinfo.host+'/member/project/'+info.id+'/detail',
+        size: 200,
+        qrcodePath: './public/upload/qrcode/'+info.id+'.png'
+    }).then(function(qrcodePath) {
+        console.log('222'+qrcodePath);  // balabala/node-qrcode/qrcode.png
+        qrcodeurl=qrcodePath;
+    });
+    console.log('1111'+qrcodeurl);  // balabala/node-qrcode/qrcode.png
 
     yield siterender(this, "project/index", {
         title: '项目详情',
